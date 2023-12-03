bace6b5b2dc20e48c0544328071c3c68eeeac2e7
Megha <meghavuppala@gmail.com>
2023-12-03 14:29:45 -0600

    updated UI for winning and loosing pages

---
+++ app/src/main/java/com/example/spoons/WinningPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.spoons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WinningPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winning_page);
+    }
+}
